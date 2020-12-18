8cbcfc69bcc0402003ff0d0fb34cb50603f6bf3e
Spencer Adams <spencer.adams93@gmail.com>
2020-12-18 17:15:27 -0600

    change the color of the navbar button

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -5,7 +5,7 @@
         <div class="absolute inset-y-0 left-0 flex items-center sm:hidden">
           <!-- Mobile menu button -->
           <button
-            class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500"
+            class="inline-flex items-center justify-center p-2 rounded-md text-indigo-400 hover:text-indigo-500 hover:bg-indigo-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500"
             aria-expanded="false"
             @click="open = !open"
           >
