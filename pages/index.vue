5b93b4ac451c86fcfe7e22999de2093991c9d525
Spencer Adams <spencer.adams93@gmail.com>
2020-12-20 01:31:07 -0600

    add icon to search

--- pages/index.vue
+++ pages/index.vue
@@ -21,14 +21,32 @@
         </svg>
       </div>
       <div class="max-w-6xl px-2 sm:px-6 lg:px-8 w-full">
-        <div class="mt-4 lg:mt-0 mb-12">
-          <label for="search" class="sr-only">Search</label>
+        <label for="search" class="sr-only">Search</label>
+        <div class="relative">
+          <div
+            class="pointer-events-none absolute inset-y-0 left-0 pl-3 flex items-center"
+          >
+            <!-- Heroicon name: search -->
+            <svg
+              class="h-5 w-5 text-gray-400"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 20 20"
+              fill="currentColor"
+              aria-hidden="true"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
+                clip-rule="evenodd"
+              />
+            </svg>
+          </div>
           <input
             id="search"
-            type="search"
             name="search"
-            class="shadow-lg focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-2 border-indigo-400 rounded-md"
+            class="shadow-lg focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-2 border-indigo-400 rounded-md py-2 pl-10 pr-3 text-sm"
             placeholder="Search for Your Favorite Dish"
+            type="search"
           />
         </div>
         <Separator text="Or Explore Our Menu" />
