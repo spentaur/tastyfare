345d5dfbbfef3af3197f7d77294f634b5853c4c4
Spencer Adams <spencer.adams93@gmail.com>
2020-12-19 21:50:54 -0600

    change footer to simple, add padding on container

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,9 @@
 <template>
   <div class="flex flex-col min-h-screen">
     <Navbar />
-    <div class="max-w-7xl mx-auto w-full h-96 border border-1-black my-12">
+    <div
+      class="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8 w-full h-96 border border-1-black my-4 sm:my-12"
+    >
       menu
     </div>
 
