82e6463b7daefb85a785a0280f8e66640d9bcf78
Spencer Adams <spencer.adams93@gmail.com>
2020-12-24 03:58:19 -0600

    change view all button size to 9

--- pages/index.vue
+++ pages/index.vue
@@ -77,7 +77,7 @@
             <nuxt-link
               v-if="section.name != 'Desserts 🍪'"
               to="#"
-              class="flex items-center justify-center w-8 h-8 font-bold text-gray-900 bg-gray-200 rounded-full shadow hover:bg-indigo-600 sm:bg-indigo-500 sm:px-3 sm:py-2 sm:h-auto sm:w-auto sm:text-white"
+              class="flex items-center justify-center font-bold text-gray-900 bg-gray-200 rounded-full shadow w-9 h-9 hover:bg-indigo-600 sm:bg-indigo-500 sm:px-3 sm:py-2 sm:h-auto sm:w-auto sm:text-white"
             >
               <div class="hidden sm:block">See all</div>
               <svg
