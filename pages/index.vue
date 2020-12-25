3541ed126553ab7fd6511a1ab250506081709bb7
Spencer Adams <spencer.adams93@gmail.com>
2020-12-24 22:58:25 -0600

    change search input color

--- pages/index.vue
+++ pages/index.vue
@@ -71,7 +71,7 @@
           <input
             id="search"
             name="search"
-            class="block w-full py-2 pl-10 pr-3 font-semibold placeholder-gray-400 transition-colors duration-100 bg-gray-200 border border-gray-200 rounded-full shadow dark:border-gray-600 dark:bg-gray-700 dark:focus:ring-gray-600 focus:ring-indigo-500 dark:focus:bg-white focus:bg-white"
+            class="block w-full py-2 pl-10 pr-3 font-semibold text-gray-400 placeholder-gray-400 transition-colors duration-100 bg-gray-200 border border-gray-200 rounded-full shadow focus:text-gray-800 dark:border-gray-600 dark:bg-gray-700 dark:focus:ring-gray-600 focus:ring-indigo-500 dark:focus:bg-white focus:bg-white"
             placeholder="Search for Your Favorites"
             type="search"
           />
