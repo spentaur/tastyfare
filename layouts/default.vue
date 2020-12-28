930d8945a0355735de60518f7d87ac7e66e58eed
Spencer Adams <spencer.adams93@gmail.com>
2020-12-28 13:47:03 -0600

    add footer back

--- layouts/default.vue
+++ layouts/default.vue
@@ -17,6 +17,7 @@
         }"
         class="pt-6 transition-all duration-300 bg-white sm:duration-300 dark:bg-gray-800 pb-22 sm:pb-12 sm:pt-22"
       />
+      <Footer />
     </div>
   </div>
 </template>
