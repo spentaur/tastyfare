419fa7cac98b544409b5e3d97e8d2adc41c9796c
Spencer Adams <spencer.adams93@gmail.com>
2020-12-28 13:42:51 -0600

    change scale to 25

--- layouts/default.vue
+++ layouts/default.vue
@@ -12,10 +12,10 @@
       <Navbar />
       <Nuxt
         :class="{
-          'translate-y-1/4 scale-0 sm:scale-100 sm:-translate-y-0 sm:translate-x-80 transform':
+          'translate-y-1/4 scale-25 sm:scale-100 sm:-translate-y-0 sm:translate-x-80 transform':
             $store.state.menu.open,
         }"
-        class="pt-6 transition-all duration-500 bg-white sm:duration-300 dark:bg-gray-800 pb-22 sm:pb-12 sm:pt-22"
+        class="pt-6 transition-all duration-300 bg-white sm:duration-300 dark:bg-gray-800 pb-22 sm:pb-12 sm:pt-22"
       />
     </div>
   </div>
