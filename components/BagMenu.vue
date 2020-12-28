d94da0b6335ed7a66fc7444be61f185412a86847
Spencer Adams <spencer.adams93@gmail.com>
2020-12-28 17:22:07 -0600

    text colors on bag menu

--- components/BagMenu.vue
+++ components/BagMenu.vue
@@ -1,10 +1,10 @@
 <template>
   <Menu name="bag">
-    {{ $store.state.bag.items }}
-    <div>
+    <div class="text-gray-900 dark:text-gray-200">
+      <div>{{ $store.state.bag.items }}</div>
       <button
         type="button"
-        class="inline-flex items-center px-6 py-2 font-semibold transition-all duration-300 bg-indigo-500 rounded-full shadow text-pink-50 hover:bg-indigo-600"
+        class="inline-flex items-center px-6 py-2 font-semibold transition-all duration-300 bg-indigo-500 rounded-full shadow focus:outline-none text-pink-50 hover:bg-indigo-600"
         @click.prevent="$store.commit('bag/empty')"
       >
         Empty Cart
