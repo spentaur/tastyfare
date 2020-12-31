ab9711008ca637015e369c49a279a360193ebab0
Spencer Adams <spencer.adams93@gmail.com>
2020-12-31 05:08:09 -0600

    v-touch not working

--- components/BagMenu.vue
+++ components/BagMenu.vue
@@ -1,19 +1,24 @@
 <template>
-  <Menu v-touch:swipe.right="$store.commit('menu/close')" name="bag">
-    <div
-      class="text-xl font-semibold text-gray-900 dark:text-gray-200 text-shadow-sm"
+  <Menu name="bag">
+    <span
+      v-touch:swipe.right="$store.commit('menu/close')"
+      class="block bg-green-100"
     >
-      Your order
-    </div>
-    <div class="text-gray-900 dark:text-gray-200">
-      <div>{{ $store.state.bag.items }}</div>
-      <button
-        type="button"
-        class="inline-flex items-center px-6 py-2 font-semibold transition-all duration-300 bg-indigo-500 rounded-full shadow focus:outline-none text-pink-50 hover:bg-indigo-600"
-        @click.prevent="$store.commit('bag/empty')"
+      <div
+        class="text-xl font-semibold text-gray-900 dark:text-gray-200 text-shadow-sm"
       >
-        Empty Cart
-      </button>
-    </div>
+        Your order
+      </div>
+      <div class="text-gray-900 dark:text-gray-200">
+        <div>{{ $store.state.bag.items }}</div>
+        <button
+          type="button"
+          class="inline-flex items-center px-6 py-2 font-semibold transition-all duration-300 bg-indigo-500 rounded-full shadow focus:outline-none text-pink-50 hover:bg-indigo-600"
+          @click.prevent="$store.commit('bag/empty')"
+        >
+          Empty Cart
+        </button>
+      </div>
+    </span>
   </Menu>
 </template>
