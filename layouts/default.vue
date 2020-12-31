2bbb363922296f3661422ac497396cc9475c7ae3
Spencer Adams <spencer.adams93@gmail.com>
2020-12-31 17:55:23 -0600

    remove tabindex, messes everything up

--- layouts/default.vue
+++ layouts/default.vue
@@ -4,8 +4,6 @@
     :class="{
       dark: $store.state.dark.enabled,
     }"
-    tabindex="0"
-    @keydown.esc="close"
   >
     <div
       class="flex flex-col min-h-screen transition-all duration-200 bg-white select-none dark:bg-gray-800"
