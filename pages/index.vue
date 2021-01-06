04e32b4a0b69b445fd3130ca611500bce5498cbd
Spencer Adams <spencer.adams93@gmail.com>
2021-01-05 22:02:47 -0600

    kinda starting from scratch

--- pages/index.vue
+++ pages/index.vue
@@ -25,51 +25,9 @@
         ></div>
       </transition>
     </div>
-
-    <div v-for="(section, index) in sections" :key="section.title">
-      <ul
-        v-if="index === 1"
-        class="grid grid-cols-1 gap-4 mt-10 mb-16 sm:grid-cols-3"
-      >
-        <Ad v-for="ad in ads" :key="ad.name" :ad="ad" />
-      </ul>
-      <div class="flex items-center mt-5">
-        <div
-          class="flex items-center flex-grow text-3xl font-extrabold text-gray-900 dark:text-gray-200 text-shadow-sm"
-        >
-          {{ section.title }}
-          <img
-            :src="`emojis/${section.emoji}.svg`"
-            class="w-8 h-8 ml-3 transition duration-75 transform-gpu hover:scale-110 hover:rotate-6 filter-logo dark:filter-logo-dark"
-            alt=""
-          />
-        </div>
-        <nuxt-link
-          :to="{
-            name: 'section',
-            params: { section: section.slug },
-          }"
-          class="flex items-center justify-center font-bold text-gray-900 transition-all duration-300 bg-gray-300 rounded-full sm:shadow-logo sm:dark:shadow-logo-dark dark:text-gray-300 dark:bg-gray-700 w-9 h-9 sm:hover:bg-indigo-600 sm:dark:bg-gray-700 sm:dark:hover:bg-gray-600 sm:bg-indigo-500 sm:px-3 sm:py-2 sm:h-auto sm:w-auto sm:text-pink-50"
-        >
-          <div class="hidden sm:block">See all</div>
-          <svg
-            class="w-4 h-4 sm:ml-2"
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-          >
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="4"
-              d="M14 5l7 7m0 0l-7 7m7-7H3"
-            />
-          </svg>
-        </nuxt-link>
-      </div>
-      <Items :items="section.items" :section="section" />
-    </div>
+    <ul class="grid grid-cols-1 gap-4 mt-10 mb-16 sm:grid-cols-3">
+      <Ad v-for="ad in ads" :key="ad.name" :ad="ad" />
+    </ul>
 
     <div
       class="w-full mt-12 mb-2 text-lg font-bold text-center text-indigo-500 dark:text-gray-200"
